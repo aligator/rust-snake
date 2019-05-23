2b8ad9de00b653d22eb006ca65b3f845a26bff77
aligator <aligator@suncraft-server.de>
2019-05-23 19:09:31 +0200

    use crossterm for input

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,65 @@
-mod logic;
 extern crate crossterm;
 
-use crossterm::*;
-use logic::field::Field;
-use logic::field::Direction;
-use std::collections::LinkedList;
 use std::{thread, time};
-use std::sync::{Mutex, Arc};
+use std::collections::LinkedList;
+use std::sync::{Arc, Mutex};
 
+use crossterm::*;
+
+use logic::field::Direction;
+use logic::field::Field;
+
+mod logic;
 
 struct Bounds {
     col: i32,
     row: i32
 }
 
-#[allow(while_true)]
 fn main() {
-    // setup ncurses
+    // setup crossterm
+    let crossterm = Crossterm::new();
+    let screen = RawScreen::into_raw_mode();
+    let terminal = crossterm.terminal();
+    let mut input = crossterm.input();
+
+    // setup field
     let bounds = init();
     let mut score: i32 = 0;
-
     let mut field = Field::new(bounds.col as usize, bounds.row  as usize, ' ', 'X', 'O', 'G', 5).expect("Illegal sizes");
-
     draw_field(&field);
 
-    let ch = Arc::new(Mutex::new(0));
-
+    // input-loop
+    let ch = Arc::new(Mutex::new(KeyEvent::Delete));
     {
-        let ch = ch.clone();
+        let ch = Arc::clone(&ch);
 
         thread::spawn(move || {
-
-            while true {
+            loop {
                 thread::sleep(time::Duration::from_millis(50));
-                let new_ch = getch();
-                let mut ch_ptr = ch.lock().unwrap();
-                *ch_ptr = new_ch;
+
+                let mut stdin = input.read_sync();
+
+                if let Some(key_event) = stdin.next() {
+                    match key_event {
+                        InputEvent::Keyboard(event) => {
+                            let new_keyEvent = event;
+
+                            let mut ch_ptr = ch.lock().unwrap();
+                            *ch_ptr = new_keyEvent;
+                        },
+                        _ => {}
+                    }
+                }
+
+
             }
         });
     }
 
     let mut end = false;
 
-    let mut last = None;
+    let mut last: Option<char> = None;
 
     while !end
     {
@@ -55,47 +72,59 @@ fn main() {
         let mut dir = match ch {
             Ok(m) => {
                 match *m {
-                    KEY_LEFT => Some(Direction::LEFT),
-                    KEY_RIGHT => Some(Direction::RIGHT),
-                    KEY_UP => Some(Direction::UP),
-                    KEY_DOWN => Some(Direction::DOWN),
+                    KeyEvent::Left => {
+                        println!("left");
+                        Some(Direction::LEFT)
+                    },
+                    KeyEvent::Right => {
+                        println!("right");
+                        Some(Direction::RIGHT)
+                    },
+                    KeyEvent::Up => {
+                        println!("up");
+                        Some(Direction::UP)
+                    },
+                    KeyEvent::Down => {
+                        println!("down");
+                        Some(Direction::DOWN)
+                    },
                     _ => None
                 }
 
             },
             _ => None
         };
+        /*
+                if dir.is_some() {
+                    last = dir.clone();
+                }
 
-        if dir.is_some() {
-            last = dir.clone();
-        }
+                if dir.is_none() {
+                    dir = last.clone();
+                }
 
-        if dir.is_none() {
-            dir = last.clone();
-        }
+                let (to_update, scored) = field.mov(dir);
 
-        let (to_update, scored) = field.mov(dir);
+                if scored {
+                    score += 1;
+                }
 
-        if scored {
-            score += 1;
-        }
-
-        match to_update {
-            Some(t) => update(t, score),
-            None => {if dir.is_some() {end = true;}}
-        }
-
+                match to_update {
+                    Some(t) => update(t, score),
+                    None => {if dir.is_some() {end = true;}}
+                }
+                */
 
     }
 
-    endwin();
+    //endwin();
 }
 
 fn draw_field(field: &Field<char>) {
     for (y, col) in field.get_field().iter().enumerate() {
         for (x, item) in col.iter().enumerate() {
             let item = *item;
-            mvaddch(y as i32, x as i32, item as chtype);
+            //mvaddch(y as i32, x as i32, item as chtype);
         }
     }
 /*
@@ -108,16 +137,16 @@ fn draw_field(field: &Field<char>) {
 
 fn update(points: LinkedList<(logic::field::Point, char)>, score: i32) {
     for (point, chr) in points {
-        mvaddch(point.1 as i32, point.0 as i32, chr as chtype);
+        //mvaddch(point.1 as i32, point.0 as i32, chr as chtype);
 
     }
     let score: &str = &score.to_string()[0..];
-    mvprintw(0, 0, score);
-    refresh();
+    //mvprintw(0, 0, score);
+    //refresh();
 }
 
 fn init() -> Bounds {
-    let locale_conf = LcCategory::all;
+    /*let locale_conf = LcCategory::all;
     setlocale(locale_conf, "de_DE.UTF-8");
 
     let window = initscr();
@@ -132,5 +161,10 @@ fn init() -> Bounds {
     Bounds {
         col: getmaxx(window),
         row: getmaxy(window)
+    }*/
+
+    Bounds {
+        col: 100,
+        row: 100
     }
 }
